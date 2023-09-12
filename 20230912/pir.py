ce5f0ff8c3be5fc30f6c3803f07217e0ba09fb4b
Randolph <dhwngjs01@naver.com>
2023-09-12 11:36:03 +0900

    UPDATE - GPIO Number
    GPIO 번호 수정 후 정상 작동하는 듯

--- 20230912/pir.py
+++ 20230912/pir.py
@@ -6,13 +6,13 @@ import RPi.GPIO as GPIO
 print(GPIO.VERSION)
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(4, GPIO.IN)
+GPIO.setup(14, GPIO.IN)
 
 def interrupt_fired(channel):
     print("interrupt fired")
     print(channel)
 
-GPIO.add_event_detect(4, GPIO.FALLING, callback=interrupt_fired)
+GPIO.add_event_detect(14, GPIO.FALLING, callback=interrupt_fired)
 
 while (True):
     time.sleep(1)
