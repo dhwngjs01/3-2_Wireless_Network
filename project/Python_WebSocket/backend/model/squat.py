4f39af299b6240bdd21f36a9003fb7e1a5bc5207
= <dhwngjs01@naver.com>
2023-12-01 16:30:41 +0900

    Fix: 스쿼트 포즈 랜드마크 부위 수정

--- project/Python_WebSocket/backend/model/squat.py
+++ project/Python_WebSocket/backend/model/squat.py
@@ -1,8 +1,8 @@
 import cv2
-import math
 import mediapipe as mp
 
 from model.pose_constants import *
+from model.calculate_angle import calculateAngle
 
 mp_pose = mp.solutions.pose
 
@@ -17,13 +17,15 @@ SQUAT_KNEE_DISTANCE_THRESHOLD = 0.1  # 무릎 간격 임계값
 def squat_detect(results, score, status):
     # 포즈 추적 결과를 그림
     if results.pose_landmarks:
-        right_knee = results.pose_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_KNEE]   # 오른쪽 무릎
-        right_hip = results.pose_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_HIP]     # 오른쪽 엉덩이
-        right_ankle = results.pose_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_ANKLE] # 오른쪽 발목
+        right_knee = results.pose_landmarks.landmark[RIGHT_KNEE]   # 오른쪽 무릎
+        right_hip = results.pose_landmarks.landmark[RIGHT_HIP]     # 오른쪽 엉덩이
+        right_ankle = results.pose_landmarks.landmark[RIGHT_ANKLE] # 오른쪽 발목
 
-        left_knee = results.pose_landmarks.landmark[mp_pose.PoseLandmark.LEFT_KNEE]     # 왼쪽 무릎
-        left_hip = results.pose_landmarks.landmark[mp_pose.PoseLandmark.LEFT_HIP]       # 왼쪽 엉덩이
-        left_ankle = results.pose_landmarks.landmark[mp_pose.PoseLandmark.LEFT_ANKLE]   # 왼쪽 발목
+        left_knee = results.pose_landmarks.landmark[LEFT_KNEE]     # 왼쪽 무릎
+        left_hip = results.pose_landmarks.landmark[LEFT_HIP]       # 왼쪽 엉덩이
+        left_ankle = results.pose_landmarks.landmark[LEFT_ANKLE]   # 왼쪽 발목
+
+        # 랜드마크마다 x, y, visibility 값이 담겨있음
 
         # 무릎 각도 계산
         right_knee_angle = calculateAngle(right_hip, right_knee, right_ankle)
@@ -62,24 +64,3 @@ def squat_detect(results, score, status):
                 score += 1
 
     return score, status
-    
-
-# 앵글 계산 함수
-# x, y, z 3개의 landmark 사이의 각도를 계산할 수 있는 함수를 생성한다.
-def calculateAngle(landmark1, landmark2, landmark3):
-    # 필요한 랜드마크 좌표를 얻어옴
-    # 이미지를 뒤집었기 때문에 x 좌표를 반전시킴 (꼼수)
-    x1, y1, _ = 1 - landmark1.x, landmark1.y, landmark1.z # 첫번째 라인 시작점
-    x2, y2, _ = 1 - landmark2.x, landmark2.y, landmark2.z # 첫번째 라인 끝점과 두번째 라인 시작점
-    x3, y3, _ = 1 - landmark3.x, landmark3.y, landmark3.z # 두번째 라인 끝점
-
-    # 세 점 사이의 각도를 계산
-    angle = math.degrees(math.atan2(y3 - y2, x3 - x2) - math.atan2(y1 - y2, x1 - x2))
-    
-    # 위 문장을 아래에 해석해줘
-    if angle < 0:
-        # 찾은 각도에 360을 더함
-        angle += 360
-
-    # 계산된 각도를 반환
-    return angle
